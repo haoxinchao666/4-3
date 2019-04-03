08cf60b4de4adc18e36a368ccbe14aeacc8997ed
haoxinchao666 <1581997517@qq.com>
2019-04-03 19:01:24 +0800

    edit

---
+++ 1.php
@@ -0,0 +1,20 @@
+<?php 
+header("content-type:text/html;charset=utf-8");
+
+$x = 2;
+$y = 6;
+echo demo($x,$y);
+function demo($x,$y)
+{
+	for ($i=0; $i <=$x ; $i++) { 
+	return $y +=$i;
+}
+
+}
+
+
+
+
+
+
+ ?>
